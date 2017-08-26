a7b51c9f960b571de5aed94f30ecf2ba6796222f
ananta pandu <anpandumail@gmail.com>
2017-08-26 10:24:25 +0700

    [deployment] scripts #2

--- shutdown.sh
+++ shutdown.sh
@@ -1,3 +1,3 @@
 docker-compose down
-docker rmi -f jenio_payment-api jenio_database jenio_pos-frontend
+docker rmi -f jenio_payment-api jenio_database
 docker volume rm $(docker volume ls -qf dangling=true)
